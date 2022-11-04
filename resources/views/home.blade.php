6147796b4c07f9888e70ae5b0173d0900f5e9c77
yudistira1231 <yudistira.belajar@gmail.com>
2022-11-04 18:35:23 +0700

    menambahkan form donation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -132,7 +132,7 @@
     {{-- bagian how to use --}}
     <div class="row">
         <div class="col-lg">
-            <h1>How to use bookmark</h1>
+            <h1>Bagaimana cara menggunakan bookmark ini</h1>
             <p>Cara menggunakan: download bookmark yang anda inginkan (dalam bentuk file html). Lalu import ke dalam bookmark yang ada di setting web browser(setiap browser mungkin memiliki sedikit perbedaan).</p>
         </div>
     </div>
@@ -141,7 +141,28 @@
         <div class="col-lg">
             <h1>Donasi sekarang</h1>
             <p>Bantu kami untuk mengembangkan bookmark terbaik dengan berdonasi sejumlah manfaat yang anda rasakan</p>
-            
+            <form action="" method="POST">
+              <div class="mb-3">
+                <label for="email" class="form-label">Alamat email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+                <div id="emailHelp" class="form-text">Kami tidak pernah membagikan email anda dengan siapa pun</div>
+              </div>
+              <div class="mb-3">
+                <label for="bank" class="form-label">Bank</label>
+                <input type="email" class="form-control" id="bank" aria-describedby="emailHelp" name="bank">
+                <div id="emailHelp" class="form-text">Pilih bank yang anda miliki.</div>
+              </div>
+              <div class="mb-3">
+                <label for="nominal" class="form-label">Nominal</label>
+                <input type="email" class="form-control" id="nominal" aria-describedby="emailHelp" name="nominal">
+                <div id="emailHelp" class="form-text">Jumlah uang yang akan anda donasikan.</div>
+              </div>
+              <div class="mb-3">
+                <label for="message" class="form-label">Tulis pesan anda</label>
+                <textarea class="form-control" id="message" rows="3" name="message"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Kirim</button>
+            </form>
         </div>
     </div>
 @endsection
