dd8ba3565c91288ea3ffd19c20069450d4a8fbdc
yudistira1231 <yudistira.belajar@gmail.com>
2022-11-07 07:26:19 +0700

    memperbaiki kembali migrate

--- database/migrations/2022_11_04_154304_create_bookmarks_table.php
+++ database/migrations/2022_11_04_154304_create_bookmarks_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string("name");
             $table->string("slug")->unique();
             $table->string("version");
-            // $table->foreignId("category_id");
+            $table->foreignId("category_id");
             $table->string("tags");
             $table->string("summary");
             $table->text("description");
