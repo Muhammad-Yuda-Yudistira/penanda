874509fa16e479fe1c8c9d3e7574ce8969d69ed8
yudistira1231 <yudistira.belajar@gmail.com>
2022-11-09 13:41:39 +0700

    menyempurnakan teks di halaman documentation

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -4,11 +4,39 @@
     <div class="row justify-content-center text-center">
         <div class="col-lg-8">
             <h1>Documentation</h1>
+            <h2>Sebelum menggunakan bookmark delete dahulu bookmark didalam web browser yang anda gunakan</h2>
+            <img src="img/documentation/bookmark menu.png" alt="bookmark menu">
             <p>
-                Cara menggunakan: download bookmark yang anda inginkan (dalam bentuk file html). Lalu import ke dalam bookmark yang ada di setting web browser(setiap browser mungkin memiliki sedikit perbedaan).
+                Step 1: Jika web browser anda sudah ada bookmarknya silahkan hapus dulu supaya tidak tercampur, kecuali anda menginginkannya.
             </p>
+            <img src="img/documentation/click bookmark manager.png" alt="click bookmark manager">
             <p>
-                Cara membagikan: import bookmark yang anda miliki di dalam browser (dalam bentuk file html). Lalu masuk ke akun anda di website kami, untuk mengelola bookmark anda. Create bookmark baru, masukan bookmark yang akan anda bagikan. Setelah selesai bookmark anda siap untuk didownload orang lain yang membutuhkan.
+                Step 2: untuk menghapus bookmark masuk ke icon titik tiga di atas kanan, pilih Bookmarks,  lalu pilih Bookmark manager. Costumize and control Google Chrome->Bookmarks->Bookmark manager.
+            </p>
+            <img src="img/documentation/bookmark manager page.png" alt="bookmark manager page">
+            <p>
+                Step 3: sekarang anda berada di dalam halaman Bookmark manager.
+            </p>
+            <img src="img/documentation/selected all bookmark and delete it.png" alt="selected all bookmark and delete it">
+            <p>
+                Step 4: seleksi semua bookmark dengan menekan tombol ctrl+a (selected all) setelah itu akan muncul tombol delete di kanan atas dan klik.
+            </p>
+            <h2>Cara import bookmark ke dalam web browser google chrome</h2>
+            <img src="img/documentation/bookmark has deleted.png" alt="bookmark has deleted">
+            <p>
+                Step 1: jika berhasil didelete halaman bookmark akan kosong. Sekarang siap untuk import bookmark.
+            </p>
+            <img src="img/documentation/import bookmark.png" alt="import bookmark">
+            <p>
+                Step 2: klik organize (icon titik tiga) di halaman bookmark lalu pilih import bookmarks.organize->import bookmark.
+            </p>
+            <img src="img/documentation/find html file for bookmark.png" alt="find html file for bookmark">
+            <p>
+                Step 3: cari bookmark yang sudah anda download berbentuk html untuk diimport dan klik open.
+            </p>
+            <img src="img/documentation/bookmark has imported.png" alt="bookmark has imported">
+            <p>
+                Step 4: jika bookmark muncul dihalaman selamat bookmark berhasil diimport dan selesai.
             </p>
         </div>
     </div>
